f9647e9ca6578153d30a62ca8dd1abb0439e80da
Larissa Rabelo <larissarabelolf@gmail.com>
2020-10-09 19:32:32 -0300

    Calculo Salario

--- 2-Android/Aula16/aula16calculohora/app/src/main/java/com/example/aula16calculohora/MainActivity.kt
+++ 2-Android/Aula16/aula16calculohora/app/src/main/java/com/example/aula16calculohora/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.aula16calculohora
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular.setOnClickListener{
+            val horasTrabalhadas = edtHorasTrabalhadas.text.toString().toInt()
+            val valorHora = edtValorHora.text.toString().toDouble()
+            val resultado = horasTrabalhadas * valorHora
+
+            txtResultado.text = "R$ $resultado"
+
+            val imm: InputMethodManager =
+                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            if (imm.isActive()) imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+        }
     }
 }
